095f178ea3c676bf6eca4be1487632acbbf3ba07
Mishon <cyke0315@gmail.com>
2024-12-28 21:54:14 +0800

    feat(ui): 添加页面过渡动画

--- fontend/src/App.vue
+++ fontend/src/App.vue
@@ -9,23 +9,42 @@ provide('globalSelect', globalSelect);
 </script>
 
 <template>
-  <router-view />
+  <router-view v-slot="{ Component }">
+    <transition name="scale" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
 
 <style>
-body, #app {
+body,
+#app {
   margin: 0;
   padding: 0;
 }
+
 :host {
   margin: 0;
   padding: 0;
 }
+.scale-enter-active, .scale-leave-active {
+  transition: transform 0.5s, opacity 0.5s;
+}
+.scale-enter-from {
+  transform: scale(0.8);
+  opacity: 0;
+}
+.scale-leave-to {
+  transform: scale(1.2);
+  opacity: 0;
+}
+
 /* 自定义字体 */
 @font-face {
   font-family: 'yousu-title-black';
   src: url('../src/assets/ttf/优设标题黑.ttf');
 }
+
 @font-face {
   font-family: 'LXGWMarkerGothic-Regular';
   src: url('../src/assets/ttf/LXGWMarkerGothic-Regular.ttf');
