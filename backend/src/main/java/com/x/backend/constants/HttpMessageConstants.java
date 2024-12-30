1783c5605f3636a0ad021413379b69bee0643d81
Mishon <cyke0315@gmail.com>
2024-12-30 14:24:30 +0800

    feat: 添加HTTP消息常量

--- backend/src/main/java/com/x/backend/constants/HttpMessageConstants.java
+++ backend/src/main/java/com/x/backend/constants/HttpMessageConstants.java
@@ -4,5 +4,12 @@ package com.x.backend.constants;
  * 此类为HTTP消息常量类
  */
 public class HttpMessageConstants {
-    public static final String SUCCESS = "success";
+    public static final String SUCCESS = "请求成功";
+    public static final String ERROR = "请求失败";
+    public static final String NOT_FOUND = "未找到资源";
+    public static final String BAD_REQUEST = "请求参数错误";
+    public static final String UNAUTHORIZED = "未授权";
+    public static final String FORBIDDEN = "无权限";
+    public static final String INTERNAL_SERVER_ERROR = "服务器内部错误";
+    public static final String SERVICE_UNAVAILABLE = "服务不可用";
 }
