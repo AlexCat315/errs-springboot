b815c7467ad09e0ea713d21318efe80675e6fceb
Mishon <cyke0315@gmail.com>
2024-12-30 14:33:00 +0800

    fix(constants): 更新HTTP状态码常量并添加Token过期状态码

--- backend/src/main/java/com/x/backend/constants/HttpCodeConstants.java
+++ backend/src/main/java/com/x/backend/constants/HttpCodeConstants.java
@@ -4,15 +4,19 @@ package com.x.backend.constants;
  * 此类为HTTP状态码常量类
  */
 public class HttpCodeConstants {
-    public static final int OK = 200; // 请求成功
+    // 请求成功
+    public static final int OK = 200;
     // 未授权
-    public static final int UNAUTHORIZED = 401;
+    public static final int UNAUTHORIZED = 4001;
     // 权限不足，拒绝访问
-    public static final int FORBIDDEN = 403;
+    public static final int FORBIDDEN = 4003;
     // 请求资源不存在
-    public static final int NOT_FOUND = 404;
+    public static final int NOT_FOUND = 4004;
+    // Token过期，已失效
+    public static final int UNAUTHORIZED_TOKEN = 4005;
     // 服务器内部错误
     public static final int INTERNAL_SERVER_ERROR = 500;
     // 服务不可用
     public static final int SERVICE_UNAVAILABLE = 503;
+
 }
