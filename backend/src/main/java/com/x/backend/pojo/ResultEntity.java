b167db33e093dd9a3a87d16954328c4b1c4048c2
Mishon <cyke0315@gmail.com>
2024-12-30 14:35:52 +0800

    feat(pojo): 使用常量替换硬编码状态码和信息

--- backend/src/main/java/com/x/backend/pojo/ResultEntity.java
+++ backend/src/main/java/com/x/backend/pojo/ResultEntity.java
@@ -3,6 +3,8 @@ package com.x.backend.pojo;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.x.backend.constants.HttpCodeConstants;
+import com.x.backend.constants.HttpMessageConstants;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -17,15 +19,15 @@ public class ResultEntity<T> implements Serializable {
 
     // 默认构造函数，成功状态
     public ResultEntity() {
-        this.code = 200;
-        this.message = "请求成功";
+        this.code = HttpCodeConstants.OK;
+        this.message = HttpMessageConstants.SUCCESS;
         this.data = null;
     }
 
     // 带数据的成功构造函数
     public ResultEntity(T data) {
-        this.code = 200;
-        this.message = "请求成功";
+        this.code = HttpCodeConstants.OK;
+        this.message = HttpMessageConstants.SUCCESS;
         this.data = data;
     }
 
@@ -53,7 +55,7 @@ public class ResultEntity<T> implements Serializable {
     }
 
     public static <T> ResultEntity<T> success(String msg, T data) {
-        return new ResultEntity<>(200, msg, data);
+        return new ResultEntity<>(HttpCodeConstants.OK, msg, data);
     }
 
     // 静态方法，用于返回失败的响应
@@ -63,12 +65,11 @@ public class ResultEntity<T> implements Serializable {
 
     // 静态方法，用于返回服务器内部错误的响应
     public static <T> ResultEntity<T> serverError() {
-        return new ResultEntity<>(500, "Server internal error", null);
+        return new ResultEntity<>(HttpCodeConstants.INTERNAL_SERVER_ERROR, HttpMessageConstants.INTERNAL_SERVER_ERROR, null);
     }
 
     // toString方法，将对象转换为json字符串，利用jackson的ObjectMapper类
-    @Override
-    public String toString() {
+    public String toJSONString() {
         try {
             return new ObjectMapper().writeValueAsString(this);
         } catch (JsonProcessingException e) {
