a4683eab2393d48a63a6ca1b8bf0435c5aa0d723
Mishon <cyke0315@gmail.com>
2024-12-30 12:31:32 +0800

    refactor(exceptionhandler): 移除冗余注解并新增RuntimeException处理

--- backend/src/main/java/com/x/backend/exceptionhandler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/x/backend/exceptionhandler/GlobalExceptionHandler.java
@@ -1,21 +1,23 @@
 package com.x.backend.exceptionhandler;
 
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import com.x.backend.pojo.ResultEntity;
-
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-@RestController
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(SecurityException.class)
     public ResultEntity<String> handleSecurityException(SecurityException e) {
         log.error("SecurityException: {}", e.getMessage());
         return ResultEntity.failure(404, e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResultEntity<String> handleRuntimeException(RuntimeException e) {
+        log.error("RuntimeException: {}", e.getMessage());
+        return ResultEntity.failure(500, e.getMessage());
     }
 }
