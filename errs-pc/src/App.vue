6eee6da602c70594a5641d6f3f098ac0de45d4f0
Mishon <cyke0315@gmail.com>
2024-12-31 18:58:33 +0800

    refactor: 移除不必要的变量并更新模板与样式

--- errs-pc/src/App.vue
+++ errs-pc/src/App.vue
@@ -1,27 +1,28 @@
 <script setup>
-import {ref} from "vue";
-import {invoke} from "@tauri-apps/api/core";
+import { ref } from "vue";
+import { invoke } from "@tauri-apps/api/core";
+import XButton from "./components/XButton.vue";
 
 const greetMsg = ref("");
-const name = ref("");
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsg.value = await invoke("greet", {name: name.value});
+  greetMsg.value = await invoke("greet", { name: "123112312" });
 }
 </script>
 
 <template>
-<div style="display: flex;">
-  <!-- 左侧菜单栏 -->
-  <div class="left-panel">
+  <div class="page" style="display: flex;">
+    <!-- 左侧菜单栏 -->
+    <div class="left-panel">
+      
+    </div>
+    <!-- 右侧内容 -->
+    <div class="right-panel">
 
+    </div>
+   
   </div>
-  <!-- 右侧内容 -->
-  <div class="right-panel">
-
-  </div>
-</div>
 </template>
 
 <style scoped>
@@ -35,10 +36,19 @@ body,
   margin: 0;
   padding: 0;
 }
+
 .left-panel {
   width: 200px;
   height: 100%;
-  background-color: #f5f5f5;
+  display: flex;
+  flex-direction: column;
+}
+
+.right-panel {
+  width: 100%;
+  height: 100vh;
+  background-color: #fff;
+  /* 四周边框圆角 */
+  border-radius: 10px;
 }
 </style>
-
