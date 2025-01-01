b46fd0b49ce9bf82e2e21c42eb4cebfe9ba7edf6
Mishon <cyke0315@gmail.com>
2025-01-01 22:09:27 +0800

    feat(net): 添加Post请求功能
    新增post.js文件，实现基于Tauri的Post请求功能。该功能支持自定义URL、请求体、成功和失败回调，并处理常见的HTTP状态码（401、403、500等）。

---
+++ errs-pc/src/net/post.js
@@ -0,0 +1,35 @@
+import {invoke} from "@tauri-apps/api/core";
+
+const baseUrl = "http://127.0.0.1:12345"
+
+export async function Post(url, body, success, failure) {
+    const headers = {"Content-Type": "application/json"};
+    try {
+        const response = await invoke("post_request", {
+            url: baseUrl + url,
+            headers: headers,
+            body: JSON.stringify(body),
+        });
+        switch (response.code) {
+            case 401:
+                // 可以跳转到登录页面
+                // router.push('/login').then(_r => {});
+                break;
+            case 403:
+                // ElMessage.warning('权限不足，无法访问');
+                break;
+            case 500:
+                // ElMessage.error('服务器内部错误');
+                break;
+            default:
+                // ElMessage.warning('发生了错误');
+        }
+        if (response.data.code === 200) {
+            success(response.data)
+        } else {
+            failure(response.data.message)
+        }
+    } catch (error) {
+        console.error("Error in Post:", error);
+    }
+}
