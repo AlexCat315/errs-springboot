80956de2734b519d82816499e54184dce3443f74
Mishon <cyke0315@gmail.com>
2025-01-06 18:20:40 +0800

    fix():修复错误

--- errs-pc/src/pages/content/Setting.vue
+++ errs-pc/src/pages/content/Setting.vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup>
 import NoLogin from './setting/NoLogin.vue';
-import { ref } from 'vue';
+import {ref} from 'vue';
+
 const isLogin = ref(false);
 </script>
 
 <template>
-<NoLogin v-if="!isLogin" />
+    <NoLogin v-if="!isLogin"/>
 </template>
 
 <style scoped lang="css">
--- errs-pc/src/pages/content/setting/NoLogin.vue
+++ errs-pc/src/pages/content/setting/NoLogin.vue
@@ -5,7 +5,6 @@
 <template>
  <div class="no-login">
    <h1>You need to login to access this page</h1>
-   <router-link to="/login">Login</router-link>
  </div>
 </template>
 
