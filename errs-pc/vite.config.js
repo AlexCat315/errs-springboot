725c7dbc1d5a2084057ba87cbd6ff9f1e2021dc5
Mishon <cyke0315@gmail.com>
2025-01-09 17:56:02 +0800

    refactor: 移除未使用的插件配置

--- errs-pc/vite.config.js
+++ errs-pc/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import AutoImport from 'unplugin-auto-import/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
+
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
@@ -11,18 +9,7 @@ export default defineConfig(async () => ({
     vue(),
     AutoImport({
       imports: [
-        "vue",
-        {
-          "naive-ui": [
-            "useDialog",
-            "useMessage",
-            "useNotification",
-            "useLoadingBar",
-          ],
-        },
-        Components({
-          resolvers: [NaiveUiResolver()],
-        }),
+        "vue"
       ],
     }),
   ],
