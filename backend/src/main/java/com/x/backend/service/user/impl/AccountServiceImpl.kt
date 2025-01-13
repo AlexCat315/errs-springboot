386f7172d783c29ef44b63caf9cc0eb678804130
Mishon <cyke0315@gmail.com>
2025-01-13 14:56:03 +0800

    fix:修复kt service 层

--- backend/src/main/java/com/x/backend/service/user/impl/AccountServiceImpl.kt
+++ backend/src/main/java/com/x/backend/service/user/impl/AccountServiceImpl.kt
@@ -4,7 +4,6 @@ import com.x.backend.mapper.user.AccountMapper
 import com.x.backend.pojo.admin.entity.Account
 import com.x.backend.service.user.AccountService
 import jakarta.annotation.Resource
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 
@@ -12,12 +11,10 @@ import org.springframework.stereotype.Service
 @Service("userAccountService")
 class AccountServiceImpl() : AccountService {
 
-    @Resource(name = "userAccountMapper")
-    lateinit var accountMapper: AccountMapper
+  @Resource(name = "userAccountMapper") lateinit var accountMapper: AccountMapper
 
   override fun findUsreByUsername(username: String): Account {
-      val user = accountMapper.findUserByUsername(username)
-      TODO("Not yet implemented")
-    }
-
+    val user: Account = accountMapper.findUserByUsername(username)
+    return user
+  }
 }
