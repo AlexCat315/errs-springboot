88251396e486a32d25810f96f6007a331d234d7a
Mishon <cyke0315@gmail.com>
2025-01-16 19:14:05 +0800

    fix(ui): 修复设置页面主题切换时标题颜色未正确更新的问题

--- errs-pc/src/pages/content/setting/Account.vue
+++ errs-pc/src/pages/content/setting/Account.vue
@@ -1,20 +1,20 @@
 <script setup lang="ts">
 
-import {inject} from "vue";
+import { inject, ref } from "vue";
 import User from "../../../components/user.vue";
 
-const globalTheme = inject("globalTheme");
-
+const globalTheme = inject<string>("globalTheme");
+const selectTheme = ref(globalTheme);
 </script>
 
 <template>
     <div>
         <!--头部设置-->
         <div style="display: flex; justify-content: space-between;">
-            <h2 style="font-family: LXGWMarkerGothic-Regular,'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            <h2 :style="{color: selectTheme === 'light' ? 'black' : '#cfd3dc'}" style="font-family: LXGWMarkerGothic-Regular,'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
             margin-top: 8px;">设置</h2>
             <div>
-                <User  />
+                <User />
             </div>
         </div>
         <div class="divider"></div>
