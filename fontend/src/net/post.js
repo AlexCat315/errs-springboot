b26c4acfed5529600a541206e74a4d82683e7929
Mishon <cyke0315@gmail.com>
2025-01-19 21:23:31 +0800

    feat(error-handling): 添加默认错误处理函数，增强API调用的错误响应处理

--- fontend/src/net/post.js
+++ fontend/src/net/post.js
@@ -3,24 +3,36 @@ import {ElMessage} from "element-plus";
 import router from "../router/router.js";
 
 
+/**
+ * 默认错误处理函数
+ * 用于处理各种API调用中遇到的错误响应
+ * @param {Object} err - 错误对象，包含响应状态码等信息
+ */
 const defaultError = (err) => {
+    // 判断是否有错误响应
     if (err.response) {
+        // 根据不同的HTTP状态码进行相应的错误处理
         switch (err.response.status) {
             case 401:
+                // 未授权错误，提示用户需要重新登录
                 ElMessage.warning('未授权，请重新登录');
                 // 可以跳转到登录页面
                 router.push('/login').then(_r => {});
                 break;
             case 403:
+                // 权限不足错误，提示用户没有访问权限
                 ElMessage.warning('权限不足，无法访问');
                 break;
             case 500:
+                // 服务器内部错误，提示具体错误信息
                 ElMessage.error('服务器内部错误');
                 break;
             default:
+                // 其他错误，提供一般性错误提示
                 ElMessage.warning('发生了错误');
         }
     } else {
+        // 网络错误，提示用户网络连接问题
         ElMessage.warning('网络错误，请稍后重试');
     }
 };
