6353a55a9c4fd2757f6bceb22a127d2732588749
AlexCat315 <cyke0315@gmail.com>
2025-03-04 01:38:48 +0800

    style: 删除未使用的导入

--- backend/src/main/java/com/x/backend/mapper/admin/CountMapper.java
+++ backend/src/main/java/com/x/backend/mapper/admin/CountMapper.java
@@ -3,7 +3,6 @@ package com.x.backend.mapper.admin;
 import java.util.Date;
 import java.util.List;
 
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
